4059f0cf66984b6f323cbf7a98590bb51e010378
Carlos Oening <carlosoening@gmail.com>
2020-12-04 22:31:41 -0300

    criado Usuario e usuarioDAO

---
+++ app/src/main/java/com/programacao/app_pizzaria/usuario/UsuarioDAO.java
@@ -0,0 +1,32 @@
+package com.programacao.app_pizzaria.usuario;
+
+import com.programacao.app_pizzaria.banco.DAO;
+
+import java.util.List;
+
+public class UsuarioDAO implements DAO<Usuario> {
+    @Override
+    public void criarTabela() {
+
+    }
+
+    @Override
+    public void adicionar(Usuario usuario) {
+
+    }
+
+    @Override
+    public List<Object> listar() {
+        return null;
+    }
+
+    @Override
+    public void atualizar(Usuario usuario) {
+
+    }
+
+    @Override
+    public void excluir(Usuario usuario) {
+
+    }
+}
